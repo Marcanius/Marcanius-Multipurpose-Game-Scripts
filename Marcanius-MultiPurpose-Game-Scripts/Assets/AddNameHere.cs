79901fbb03ab4aaf3f18a5030b62a06e97098b3e
Matthijs Platenburg <Mattieplatenburg@gmail.com>
2018-01-25 17:15:55 +0100

    First Upload
    Added Sample Unity Project
    Added Input Manager
    Added Empty Class

---
+++ Marcanius-MultiPurpose-Game-Scripts/Assets/AddNameHere.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddNameHere : MonoBehaviour {
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Awake() {
+
+	}
+
+	/// <summary>
+	/// Use this as a first update
+	/// </summary>
+	void Start() {
+
+	}
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update() {
+
+	}
+}
---
+++ Marcanius-MultiPurpose-Game-Scripts/Assets/Input Manager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	iInputSystem instance;
+
+	void Awake() {
+
+	}
+
+	/// <summary>
+	/// Returns the right input system for this platform
+	/// </summary>
+	public iInputSystem GetInputSystem() {
+		if ( instance == null ) { instance = CreateInstance(); }
+
+		return instance;
+	}
+
+	/// <summary>
+	/// Creates a new instance of the right input system for this platform
+	/// </summary>
+	static iInputSystem CreateInstance() {
+#if UNITY_STANDALONE
+		throw new NotImplementedException();
+#endif
+	}
+}
+
+public enum GameAction { }
+
+public interface iInputSystem {
+	bool GetInput(GameAction action);
+	Vector3 GetCursorPosition();
+	Vector3 GetCursorWorldPosition(float depth = 0);
+}
+
+public class StandaloneInputSystem : iInputSystem {
+	public bool GetInput(GameAction action) {
+		return Input.GetButtonDown(action.ToString());
+	}
+
+	public Vector3 GetCursorPosition() {
+		return Input.mousePosition;
+	}
+
+	public Vector3 GetCursorWorldPosition(float depth = 0) {
+		return new Vector3();
+	}
+}
